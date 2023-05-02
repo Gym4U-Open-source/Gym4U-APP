1b5abca472686be1d7b3e0fba04a9c9328ce99d0
Jerry Quispe Gavilan <U20201c043@upc.edu.pe>
2023-05-02 18:17:11 -0500

    aparacion de pantallas flotantes

--- app/src/main/java/com/example/gym4u_movile_app/LibraryFragment.kt
+++ app/src/main/java/com/example/gym4u_movile_app/LibraryFragment.kt
@@ -1,10 +1,15 @@
 package com.example.gym4u_movile_app
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupWindow
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +39,57 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_library, container,false)
+
+        val btn_add_new_workouts = view.findViewById<Button>(R.id.btn_add_new_workouts)
+        btn_add_new_workouts.setOnClickListener {
+            val popupWindow1 = PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+            val popupView1 = layoutInflater.inflate(R.layout.activity_new_workout, null)
+            popupWindow1.contentView = popupView1
+
+            val view1 = view.findViewById<View>(R.id.frameLayout2)
+            view1.setBackgroundColor(Color.parseColor("#99000000"))
+
+            val btnAddWorkout= popupView1.findViewById<Button>(R.id.btn_add_workout)
+            btnAddWorkout.setOnClickListener {
+                view1.setBackgroundColor(Color.parseColor("#ffffff"))
+                popupWindow1.dismiss()
+            }
+
+            popupWindow1.width = ViewGroup.LayoutParams.WRAP_CONTENT
+            popupWindow1.height  = ViewGroup.LayoutParams.WRAP_CONTENT
+            popupWindow1.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
+            popupWindow1.showAtLocation(btn_add_new_workouts, Gravity.NO_GRAVITY or Gravity.CENTER_VERTICAL,10,0)
+
+            //segunda ventana flotante
+
+            val popupView2 = LayoutInflater.from(requireContext()).inflate(R.layout.activity_add_exercise, null)
+            val popupWindow2 = PopupWindow(popupView2, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+
+            popupWindow2.contentView = popupView2
+
+            popupWindow2.width = ViewGroup.LayoutParams.WRAP_CONTENT
+            popupWindow2.height = 1800
+
+            val btnAddExercises = popupView1.findViewById<Button>(R.id.btn_add_new_exercises)
+            btnAddExercises.setOnClickListener {
+                val view2 = popupView1.findViewById<View>(R.id.vAddWorkout)
+                view2.setBackgroundColor(Color.parseColor("#99000000"))
+                popupWindow2.showAsDropDown(btnAddExercises)
+                popupWindow2.showAtLocation(view, Gravity.CENTER,0,0)
+
+                val btnAddExercise = popupView2.findViewById<Button>(R.id.btn_add_exercise)
+                btnAddExercise.setOnClickListener {
+                    view2.setBackgroundColor(Color.parseColor("#ffffff"))
+                    popupWindow2.dismiss()
+                 }
+
+            }
+
+        }
+
+        return view
     }
 
     companion object {
@@ -56,4 +111,6 @@ class LibraryFragment : Fragment() {
                 }
             }
     }
+
+
 }
---
+++ app/src/main/java/com/example/gym4u_movile_app/new_workout.kt
@@ -0,0 +1,11 @@
+package com.example.gym4u_movile_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_workout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_workout)
+    }
+}
