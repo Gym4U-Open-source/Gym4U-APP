a9f9269bb2ac2ef97604983950de9b9a15e39249
JuanPabloPT2001 <u201915206@gmail.com>
2023-06-01 12:42:08 -0500

    feat: Visualize client added

---
+++ app/src/main/java/com/example/gym4u_movile_app/entities/Client.kt
@@ -0,0 +1,5 @@
+package com.example.gym4u_movile_app.entities
+
+class Client (
+    val name: String
+    )
--- app/src/main/java/com/example/gym4u_movile_app/ui/clients/ClientAdapter.kt
+++ app/src/main/java/com/example/gym4u_movile_app/ui/clients/ClientAdapter.kt
@@ -3,9 +3,13 @@ package com.example.gym4u_movile_app.ui.clients
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gym4u_movile_app.R
+import com.example.gym4u_movile_app.entities.Client
 
 class ClientAdapter(var clients: ArrayList<Client>): RecyclerView.Adapter<ClientPrototype>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientPrototype {
@@ -19,13 +23,25 @@ class ClientAdapter(var clients: ArrayList<Client>): RecyclerView.Adapter<Client
 
     override fun onBindViewHolder(holder: ClientPrototype, position: Int) {
         holder.bind(clients.get(position))
+
+
+        holder.btView.setOnClickListener{
+            val clientFragment = ClientFragment()
+            val fragmentManager = (holder.itemView.context as AppCompatActivity).supportFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.btView, clientFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+         
     }
 
 }
 
 class ClientPrototype(itemView: View): RecyclerView.ViewHolder(itemView) {
     val tvName = itemView.findViewById<TextView>(R.id.tvName)
-
+    val btView = itemView.findViewById<ImageButton>(R.id.btView)
     fun bind(client: Client){
         tvName.text = client.name
     }
