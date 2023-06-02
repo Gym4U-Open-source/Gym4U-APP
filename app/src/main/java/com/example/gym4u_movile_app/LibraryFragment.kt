1e66ea564eab053c121e8a57fae949055cc9afa6
QuispeJerry <u20201c043@upc.edu.pe>
2023-06-02 00:33:40 -0500

    se agregó la ventana principal donde se divide la ventanaWorkouts y Exercises

--- app/src/main/java/com/example/gym4u_movile_app/LibraryFragment.kt
+++ app/src/main/java/com/example/gym4u_movile_app/LibraryFragment.kt
@@ -4,24 +4,12 @@ package com.example.gym4u_movile_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.gym4u_movile_app.entities.BaseResponse
-import com.example.gym4u_movile_app.entities.WORKOUTT
-import com.example.gym4u_movile_app.services.WorkoutService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,10 +26,7 @@ class LibraryFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    var workouts = ArrayList<WORKOUTT>();//1
-    var adapterWorkout = WORKOUTSAdapter(workouts);//2
-    private lateinit var handler: Handler
-    private lateinit var updateTextRunnable: Runnable
+
     private var textView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,74 +41,21 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_library, container,false)
-        loadData()
-        initView(view)
-        val tvCount = view.findViewById<TextView>(R.id.countWorkouts)
-        //tvCount.text = workouts.size.toString()
-        handler = Handler()
-        updateTextRunnable = object : Runnable {
-            override fun run() {
-                // Actualiza el contenido del TextView
-                tvCount?.text = workouts.size.toString()
 
-                // Programa la próxima actualización después de un intervalo de tiempo
-                handler.postDelayed(this, 1000) // 1000 ms = 1 segundo
-            }
+        val tvPrueba = view.findViewById<TextView>(R.id.tvExercise)
+        tvPrueba.setOnClickListener{
+            //val intenttt = Intent(activity, add_exercise::class.java)
+            //startActivity(intenttt)
         }
 
-
-            super.onStart()
-
-            // Inicia la primera actualización después de un intervalo de tiempo
-            handler.postDelayed(updateTextRunnable, 1000) // 1000 ms = 1 segundo
-
+        val tvPrueba1 = view.findViewById<TextView>(R.id.tvWorkouts)
+        tvPrueba1.setOnClickListener{
+            val intentt = Intent(activity, List_Workout::class.java)
+            startActivity(intentt)
+        }
 
         return view
     }
-
-    private fun initView(view: View) {
-        val btn_add_new_workouts = view.findViewById<Button>(R.id.btn_add_new_workouts)
-        val recyclerViewAdapter = view.findViewById<RecyclerView>(R.id.rvWorkouts)
-        val layoutManager = LinearLayoutManager(context)
-
-
-        adapterWorkout = WORKOUTSAdapter(workouts);
-        recyclerViewAdapter.layoutManager = layoutManager
-        recyclerViewAdapter.adapter = adapterWorkout
-        btn_add_new_workouts.setOnClickListener{
-            val intent = Intent(activity, new_workout::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun loadData() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://gym4u-api-388317.rj.r.appspot.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val exerciseService: WorkoutService = retrofit.create(WorkoutService:: class.java)
-        val request = exerciseService.getAll()
-
-        request.enqueue(object: Callback<BaseResponse<WORKOUTT>>{
-            override fun onResponse(
-                call: Call<BaseResponse<WORKOUTT>>,
-                response: Response<BaseResponse<WORKOUTT>>
-            ) {
-                response.body()!!.content.forEach {
-                    workouts.add(it)
-                }
-
-                adapterWorkout.notifyDataSetChanged()
-                Log.d("Exercise", workouts[0].id.toString())
-            }
-
-            override fun onFailure(call: Call<BaseResponse<WORKOUTT>>, t: Throwable) {
-                Log.d("aaaa", t.toString())
-            }
-        })
-    }
-
 
     companion object {
         /**
---
+++ app/src/main/java/com/example/gym4u_movile_app/List_Workout.kt
@@ -0,0 +1,100 @@
+package com.example.gym4u_movile_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gym4u_movile_app.entities.BaseResponse
+import com.example.gym4u_movile_app.entities.WORKOUTT
+import com.example.gym4u_movile_app.services.WorkoutService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class List_Workout : AppCompatActivity() {
+
+    var workouts = ArrayList<WORKOUTT>();//1
+    var adapterWorkout = WORKOUTSAdapter(workouts);//2
+    //private lateinit var handler: Handler
+    //private lateinit var updateTextRunnable: Runnable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_workout)
+
+        loadData()
+        initView()
+
+
+        val btn_add_new_workouts = findViewById<Button>(R.id.btn_add_new_workouts2)
+        btn_add_new_workouts.setOnClickListener{
+            val intent = Intent(this, new_workout::class.java)
+            startActivity(intent)
+        }
+
+//        val tvCount = findViewById<TextView>(R.id.countWorkouts2)
+//        //tvCount.text = workouts.size.toString()
+//        handler = Handler()
+//        updateTextRunnable = object : Runnable {
+//            override fun run() {
+//                // Actualiza el contenido del TextView
+//                tvCount?.text = workouts.size.toString()
+//
+//                // Programa la próxima actualización después de un intervalo de tiempo
+//                handler.postDelayed(this, 1000) // 1000 ms = 1 segundo
+//            }
+//        }
+//
+//        super.onStart()
+//
+//        // Inicia la primera actualización después de un intervalo de tiempo
+//        handler.postDelayed(updateTextRunnable, 1000) // 1000 ms = 1 segundo
+    }
+
+    private fun initView() {
+
+        val recyclerViewAdapter = findViewById<RecyclerView>(R.id.rvWorkouts)
+        val layoutManager = LinearLayoutManager(this)
+
+        adapterWorkout = WORKOUTSAdapter(workouts);
+        recyclerViewAdapter.layoutManager = layoutManager
+        recyclerViewAdapter.adapter = adapterWorkout
+
+    }
+
+    private fun loadData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://gym4u-api-388317.rj.r.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val exerciseService: WorkoutService = retrofit.create(WorkoutService:: class.java)
+        val request = exerciseService.getAll()
+
+        request.enqueue(object: Callback<BaseResponse<WORKOUTT>> {
+            override fun onResponse(
+                call: Call<BaseResponse<WORKOUTT>>,
+                response: Response<BaseResponse<WORKOUTT>>
+            ) {
+                response.body()!!.content.forEach {
+                    workouts.add(it)
+                }
+
+                adapterWorkout.notifyDataSetChanged()
+                Log.d("Exercise", workouts[0].id.toString())
+            }
+
+            override fun onFailure(call: Call<BaseResponse<WORKOUTT>>, t: Throwable) {
+                Log.d("aaaa", t.toString())
+            }
+        })
+    }
+}
