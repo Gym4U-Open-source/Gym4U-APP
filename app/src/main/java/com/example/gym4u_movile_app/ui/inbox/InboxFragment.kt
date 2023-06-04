bc57d8ac63dc2685ca028baa73a5247955a46533
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-06-04 14:07:17 -0500

    feat: added filter followers

--- app/src/main/java/com/example/gym4u_movile_app/ui/inbox/InboxFragment.kt
+++ app/src/main/java/com/example/gym4u_movile_app/ui/inbox/InboxFragment.kt
@@ -1,6 +1,10 @@
 package com.example.gym4u_movile_app.ui.inbox
 
+import FollowersAdapter
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +14,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gym4u_movile_app.databinding.FragmentInboxBinding
 import com.example.gym4u_movile_app.entities.BaseResponse
 import com.example.gym4u_movile_app.entities.Follower
+import com.example.gym4u_movile_app.entities.FollowerUser
 import com.example.gym4u_movile_app.services.FollowerService
 import com.example.gym4u_movile_app.util.RetrofitBuilder
+import com.example.gym4u_movile_app.util.UtilFn.Companion.textContainAnyCase
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +26,8 @@ class InboxFragment : Fragment() {
 
 
     private lateinit var binding: FragmentInboxBinding
+    private val followers = ArrayList<Follower>()
+    private val filteredFollowers = ArrayList<Follower>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,12 +35,40 @@ class InboxFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentInboxBinding.inflate(inflater, container, false)
-        loadMessages()
+        loadViews()
+        loadFollowers()
         return binding.root
     }
 
+    private fun loadViews() {
+        binding.rvChats.layoutManager = LinearLayoutManager(context)
+        binding.rvChats.adapter = FollowersAdapter(filteredFollowers)
 
-    private fun loadMessages() {
+        binding.etChatFilter.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                search(s?.toString())
+            }
+        })
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun search(keyword: String?) {
+        filteredFollowers.clear()
+        filteredFollowers.addAll(followers)
+        if(keyword != null)
+            filteredFollowers.removeIf { follower -> !textContainAnyCase(follower.clientUser.username, keyword) && !textContainAnyCase(follower.clientUser.email, keyword) }
+
+        binding.rvChats.adapter?.notifyDataSetChanged()
+    }
+
+    private fun loadFollowers() {
         RetrofitBuilder.build()
             .create(FollowerService::class.java)
             .getFollowers(2)
@@ -42,8 +78,11 @@ class InboxFragment : Fragment() {
                     response: Response<BaseResponse<Follower>>
                 ) {
                     if(response.isSuccessful) {
-                        binding.rvChats.layoutManager = LinearLayoutManager(context)
-                        binding.rvChats.adapter = InboxAdapter(response.body()!!.content)
+                        response.body()?.content?.let {
+                            filteredFollowers.addAll(it)
+                            followers.addAll(it)
+                            binding.rvChats.adapter?.notifyItemRangeInserted(0, followers.size)
+                        }
                     }
 
                 }
